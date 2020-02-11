33d80aa58f06857cecdc8a4fdcb26b6c6e600509
Bala <bala_gunasekaran@infosys.com>
2020-02-11 11:29:20 +0530

    Task 1-B - Added accessability features

--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
@@ -3,6 +3,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
 import { TIMEPERIODS } from './stock.constants';
 
+export enum FormStatus {
+  Initial,
+  Error
+}
+
 @Component({
   selector: 'coding-challenge-stocks',
   templateUrl: './stocks.component.html',
@@ -12,6 +17,8 @@ export class StocksComponent implements OnInit {
   stockPickerForm: FormGroup;
   symbol: string;
   period: string;
+  readonly FormStatus = FormStatus;
+  formStatus = FormStatus.Initial;
 
   quotes$ = this.priceQuery.priceQueries$;
 
@@ -24,12 +31,34 @@ export class StocksComponent implements OnInit {
     });
   }
 
+  get symbolIsInvalid() {
+    return (this.stockPickerForm as FormGroup).controls.symbol.invalid;
+  }
+  get symbolIsInvalidAndTouched() {
+    return (
+      this.symbolIsInvalid &&
+      (this.stockPickerForm as FormGroup).controls.symbol.touched
+    );
+  }
+
+  get periodIsInvalid() {
+    return (this.stockPickerForm as FormGroup).controls.period.invalid;
+  }
+  get periodIsInvalidAndTouched() {
+    return (
+      this.periodIsInvalid &&
+      (this.stockPickerForm as FormGroup).controls.period.touched
+    );
+  }
+
   ngOnInit() {}
 
   fetchQuote() {
     if (this.stockPickerForm.valid) {
       const { symbol, period } = this.stockPickerForm.value;
       this.priceQuery.fetchQuote(symbol, period);
+    } else {
+      this.formStatus = FormStatus.Error;
     }
   }
 }
